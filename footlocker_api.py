5fb7207a2d1236c1a47f462c4ad7356db530a7d3
Tian Zeng <t3zeng@gmail.com>
2021-02-14 17:31:32 -0800

    check countdown time

--- footlocker_api.py
+++ footlocker_api.py
@@ -1,8 +1,22 @@
 import http_helper
+import re
+import time
 
 http = http_helper.HTTP()
 
 def footlocker_buy_product(product_url):
     url = product_url
     response = http.get(url)
-    print(response)
+
+    ts = int(round(time.time() * 1000))
+
+    countdown_data = re.search('"displayCountDownTimer":true,"skuLaunchDate":(\d+)', response)
+    if(countdown_data != None):
+        time_left = int((int(countdown_data.group(1))-ts)/1000)
+        print("Product unreleased. Waiting "+str(time_left)+" seconds")
+        time.sleep(time_left)
+    else:
+        print("Product is released, BUY BUY BUY BUY!")
+    
+    
+    # print(response)
